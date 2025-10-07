4c2f657b98ceb3a5d856575e8d1eba43c7651192
juanestodo500 <juanestodo500@gmail.com>
2025-10-06 23:14:21 -0500

    Sistemas Home Layout Vistas
    sistema des dashboard para presentar las diferentes módulos

--- ERP.Blazor/obj/Debug/net8.0/ERP.Blazor.AssemblyInfo.cs
+++ ERP.Blazor/obj/Debug/net8.0/ERP.Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0486bd508cb118d795d9ddcd85704710b19d6a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146d3cf91ea82b4e78116c4c2538fc2739930191")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
